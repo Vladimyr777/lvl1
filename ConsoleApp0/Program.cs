c5612239ad4a8c9723032026d4dc7d3bd8c79a75
Vladimir <vocap87@gmail.com>
2021-04-29 12:20:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как тебя зовут? ") ;
+            var name = Console.ReadLine() ;
+            Console.WriteLine($"Сегодня [{DateTime.Now}] , приветсвую {name}");
+        }
+    }
+}
